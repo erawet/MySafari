e923c47a937894c711012b4a78be83cf71182557
Don Wettasinghe <donwettasinghe@Dons-MacBook-Pro.local>
2015-01-09 19:42:06 -0500

    Initial Commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by Don Wettasinghe on 1/9/15.
+//  Copyright (c) 2015 Don Wettasinghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySafari/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MySafari
+//
+//  Created by Don Wettasinghe on 1/9/15.
+//  Copyright (c) 2015 Don Wettasinghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
